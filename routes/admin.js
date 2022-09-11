0bcd46200c875a84c52c9140e678eca9a85cda88
Aditya Prakash <adityaprakash@Adityas-Air.bbrouter>
2022-09-11 22:57:37 +0530

    delete from cart

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,6 @@ const router = express.Router();
 // /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-
 // /admin/products => GET
 router.get('/products', adminController.getProducts);
 
@@ -18,8 +17,8 @@ router.post('/add-product', adminController.postAddProduct);
 
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
-router.post('/edit-product', adminController.postEditProduct)
+router.post('/edit-product', adminController.postEditProduct);
 
-router.post('/delete-product', adminController.postDeleteProduct)
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
